827ef30145241713ee75d11a3fa1b3c2ded755d0
Sam Logan <sam@birdbrain.com.au>
2017-03-24 12:29:17 +1100

    External class check added to navigation, closes #42

--- app/components/Common/Navigation.jsx
+++ app/components/Common/Navigation.jsx
@@ -7,13 +7,21 @@ export default class Navigation extends Component {
     const isActive = getSlug(item.url) === currentPath;
     return isActive ? `active ${item.classes}` : `${item.classes}`;
   }
+  getLink(item) {
+    // External Link Case
+    if (item.classes && item.classes.indexOf('external') !== -1) {
+      return <a href={`${getSlug(item.url)}`} target="_blank" rel="noopener noreferrer">{item.title.replace(/&#038;/g, '&')}</a>;
+    }
+    // Internal Link Case
+    return <Link to={`${getSlug(item.url)}`}>{item.title.replace(/&#038;/g, '&')}</Link>;
+  }
   renderSubNavigation(subItems, currentPath) {
     return (
       <ul>
         {subItems.map((subItem, index) => (
           <li key={index} className={this.getClassNames(subItem, currentPath)}>
             <Link to={`${getSlug(subItem.url)}`}>
-              {subItem.title}
+              {this.getLink(subItem)}
             </Link>
           </li>
         ))}
@@ -28,7 +36,7 @@ export default class Navigation extends Component {
           {items.map((item, index) => (
             <li key={index} className={this.getClassNames(item, currentPath)}>
               <Link to={`${getSlug(item.url)}`}>
-                {item.title}
+                {this.getLink(item)}
               </Link>
               {item.children && item.children.length > 0 ? this.renderSubNavigation(item.children, currentPath) : null}
             </li>
