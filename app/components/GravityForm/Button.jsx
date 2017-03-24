7a09e05f22b644a588498a2e2fdbebf208e15baa
Sam Logan <sam@birdbrain.com.au>
2017-03-24 11:21:43 +1100

    GF button classname updated, closes #32

--- app/components/GravityForm/Button.jsx
+++ app/components/GravityForm/Button.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export const Button = props => {
   const { text, showLoading, className } = props;
   return (
-    <button type="submit" className={['button', className]}>{showLoading ? 'Loading' : text}</button>
+    <button type="submit" className={`button ${className}`}>{showLoading ? 'Loading' : text}</button>
   );
 };
