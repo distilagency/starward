dc97ac36a379f98e9ddd8ad2767990bf09d41f82
Allan Pooley <allanpooley@outlook.com>
2017-07-14 16:28:49 +1000

    Implemented action types and extended starward reducer for making starward state updates on the fly

--- app/reducers/starward.js
+++ app/reducers/starward.js
@@ -1,5 +1,8 @@
 import {
   REQUEST_SUCCESS,
+  UPDATE_STARWARD_STATE,
+  UPDATE_STARWARD_STATE_SUCCESS,
+  UPDATE_STARWARD_STATE_FAILURE
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -13,6 +16,12 @@ export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
   case REQUEST_SUCCESS:
     return { ...state, ...action.payload };
+  case UPDATE_STARWARD_STATE:
+    return { ...state, starwardUpdating: true };
+  case UPDATE_STARWARD_STATE_SUCCESS:
+    return { ...state, starwardUpdating: false, [action.state]: action.payload };
+  case UPDATE_STARWARD_STATE_FAILURE:
+    return { ...state, starwardUpdating: false };
   default:
     return state;
   }
