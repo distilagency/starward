a44dd5ef30249c7bec3cfafd4ee68e1166a3ed1d
Michael Crook <michael.k.crook@gmail.com>
2017-07-25 16:48:12 +0800

    removed config files from source control

--- server/config/app.js
+++
@@ -1,8 +0,0 @@
-// Wordpress
-export const WP_AUTH = new Buffer('root:root').toString('base64');
-
-// Redis
-export const REDIS_PREFIX = 'starward-'; // change me to your project name, for example: 'starward-'
-
-// General
-export const serversideStateCharacterBlacklistRegex = /\u2028/g;
