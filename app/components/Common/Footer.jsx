e828472df190ee4cdab29c9a802f3786862ddadc
Michael Crook <michael.k.crook@gmail.com>
2017-07-25 16:39:05 +0800

    removed test code

--- app/components/Common/Footer.jsx
+++ app/components/Common/Footer.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import moment from 'moment';
-import Navigation from './Navigation';
-
-
-// remove me
-import GravityForm from '../GravityForm/Form'
 
 export const Footer = props => {
   const { siteName } = props;
   return (
     <footer className="content-info" role="contentinfo">
-      <GravityForm formId="1" />
       <p>Copyright {siteName} {moment().format('YYYY')}</p>
     </footer>
   );
