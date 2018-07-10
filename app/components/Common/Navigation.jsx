d8633c59b6ccb9f8b7500442c764579b7f6fd96f
samlogan <sam@sjlwebdesign.com>
2018-07-11 08:06:49 +1000

    Navigation tidy up

--- app/components/Common/Navigation.jsx
+++ app/components/Common/Navigation.jsx
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
-export default class Navigation extends Component {
-  getLink(item) {
-    // External Link Case
-    if (item.classes && item.classes.indexOf('external') !== -1) {
-      return <a href={item.url} target="_blank" rel="noopener noreferrer" dangerouslySetInnerHTML={{__html: item.title}} />;
-    }
-    // Internal Link Case
-    return <NavLink to={item.url} activeClassName="active" dangerouslySetInnerHTML={{__html: item.title}} />;
-  }
-  renderSubNavigation(subItems) {
+const NavigationLink = ({ item }) => {
+  // External Link Case
+  if (item.classes && item.classes.indexOf('external') !== -1) {
     return (
-      <ul>
-        {subItems.map((subItem, index) => (
-          <li key={index} className={`${subItem.classes}`}>
-            {this.getLink(subItem)}
-          </li>
-        ))}
-      </ul>
+      <a
+        href={item.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        dangerouslySetInnerHTML={{__html: item.title}}
+      />
     );
   }
+  // Internal Link Case
+  return (
+    <NavLink
+      to={item.url}
+      activeClassName="active"
+      aria-current="true"
+      dangerouslySetInnerHTML={{__html: item.title}}
+    />
+  );
+}
+
+const SubNavigation = ({ children }) => {
+  return (
+    <ul>
+      {children.map(child => (
+        <li key={child.order} className={`${child.classes}`}>
+          <NavigationLink item={child} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default class Navigation extends Component {
   render() {
     const { items } = this.props;
     return (
       <nav className="nav_banner" role="navigation">
         <ul>
-          {items.map((item, index) => (
-            <li key={index} className={`${item.classes}`}>
-              {this.getLink(item)}
-              {item.children && item.children.length > 0 ? this.renderSubNavigation(item.children) : null}
+          {items.map(item => (
+            <li key={item.order} className={`${item.classes}`}>
+              <NavigationLink item={item} />
+              {item.children && item.children.length > 0 && <SubNavigation children={item.children} />}
             </li>
           ))}
         </ul>
