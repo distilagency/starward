4dd61c901df2388167d2f98462526cb75b7d8f53
Allan Pooley <allanpooley@outlook.com>
2018-07-24 16:43:40 +1000

    Better code reuse between products & related products

--- app/components/Product/RelatedProducts.jsx
+++ app/components/Product/RelatedProducts.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { STORE_PRODUCTS_SLUG } from '../../config/app';
+import { ProductListItem } from '../Products/ProductListItem';
+// import { NavLink } from 'react-router-dom';
+// import { STORE_PRODUCTS_SLUG } from '../../config/app';
 
-const RelatedProductsList = (props) => {
-  const { relatedProducts } = props;
-  return relatedProducts.map((relatedProduct) => {
-      const {
-        images,
-        id,
-        name,
-        regular_price: regularPrice,
-        sale_price: salePrice,
-        slug,
-      } = relatedProduct;
-      const relatedProductBaseImage = (images && images.length > 0) ? images[0] : null;
-      const price = salePrice ? <span className="price"><s>{`$${regularPrice}`}</s>{` ${salePrice}`}</span> : `$${regularPrice}`;
-      return (
-        <li className="related-product" key={id}>
-          <NavLink to={`/${STORE_PRODUCTS_SLUG}/${slug}`}>
-            { relatedProductBaseImage && <img src={`${relatedProductBaseImage.src}`} alt={relatedProductBaseImage.alt} />}
-            <h3 className="title">{name}</h3>
-            <div className="price">{price}</div>
-          </NavLink>
-        </li>
-      );
-    });
-};
+// const RelatedProductsList = (props) => {
+//   const { relatedProducts } = props;
+//   return relatedProducts.map((relatedProduct) => {
+//       const {
+//         images,
+//         id,
+//         name,
+//         regular_price: regularPrice,
+//         sale_price: salePrice,
+//         slug,
+//       } = relatedProduct;
+//       const relatedProductBaseImage = (images && images.length > 0) ? images[0] : null;
+//       const price = salePrice ? <span className="price"><s>{`$${regularPrice}`}</s>{` ${salePrice}`}</span> : `$${regularPrice}`;
+//       return (
+//         <li className="related-product" key={id}>
+//           <NavLink to={`/${STORE_PRODUCTS_SLUG}/${slug}`}>
+//             { relatedProductBaseImage && <img src={`${relatedProductBaseImage.src}`} alt={relatedProductBaseImage.alt} />}
+//             <h3 className="title">{name}</h3>
+//             <div className="price">{price}</div>
+//           </NavLink>
+//         </li>
+//       );
+//     });
+// };
 
 export function RelatedProducts({ relatedProducts }) {
   if (!relatedProducts || relatedProducts.length < 1) return null;
@@ -33,7 +34,7 @@ export function RelatedProducts({ relatedProducts }) {
     <section className="related-products">
       <h2>Related Products</h2>
       <ul className="related-products-list">
-        <RelatedProductsList relatedProducts={relatedProducts} />
+        {relatedProducts.map(relatedProduct => <ProductListItem {...relatedProduct} />)}
       </ul>
     </section>
   );
