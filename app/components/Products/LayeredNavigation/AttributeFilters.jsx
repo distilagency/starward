6d711e6f80e8be6342d5584154a7dc03c0f0d7ff
Allan Pooley <allanpooley@outlook.com>
2018-07-26 17:03:06 +1000

    Strong query string logic

--- app/components/Products/LayeredNavigation/AttributeFilters.jsx
+++ app/components/Products/LayeredNavigation/AttributeFilters.jsx
@@ -4,20 +4,31 @@ import { NavLink } from 'react-router-dom';
 const AttributeFilter = (props) => {
   const selectFilter = (event, slug, termId) => {
     event.preventDefault();
-    const { changeQueryHandler } = props;
-    changeQueryHandler({ [slug]: termId });
+    const { toggleQueryHandler } = props;
+    toggleQueryHandler(slug, termId);
   };
-  const { attribute } = props;
+  const { attribute, currentParams } = props;
   if (!attribute.options) return null;
   return (
     <div className={`filter-block ${attribute.slug}`}>
       <span className="title">{attribute.label}</span>
-      <ul>
+      <ul className="options">
         { attribute.options &&
           attribute.options.map((option) => {
+            const termId = option.term_id.toString();
+            const keyExists = Object.prototype.hasOwnProperty.call(currentParams, attribute.slug);
+            const isArray = keyExists && Array.isArray(currentParams[attribute.slug]);
+            let isActive = false;
+            if (keyExists && isArray) {
+             isActive = currentParams[attribute.slug].includes(termId);
+           } else if (keyExists && !isArray) {
+             isActive = currentParams[attribute.slug] === termId;
+           }
             return (
-              <li key={option.name}>
-                <NavLink to="#" onClick={event => selectFilter(event, attribute.slug, option.term_id)}>{option.name}</NavLink>
+              <li className={`option ${isActive ? 'active' : ''}`} key={option.name}>
+                <NavLink to="#" onClick={event => selectFilter(event, attribute.slug, option.term_id)}>
+                  <span>{option.name}</span>
+                </NavLink>
               </li>
             );
         })}
@@ -30,15 +41,17 @@ export const AttributeFilters = (props) => {
   const {
     filters,
     filterType,
-    changeQueryHandler
+    currentParams,
+    toggleQueryHandler
   } = props;
   return Object.keys(filters[filterType]).map((attribute) => {
     const attributeDetails = filters[filterType][attribute];
     return (
       <AttributeFilter
-        attribute={attributeDetails}
-        changeQueryHandler={changeQueryHandler}
         key={attributeDetails.slug}
+        attribute={attributeDetails}
+        currentParams={currentParams}
+        toggleQueryHandler={toggleQueryHandler}
       />
     );
   });
