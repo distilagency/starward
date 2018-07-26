21c491d6d7918c34c099ccd56b03530f804a7d67
Allan Pooley <allanpooley@outlook.com>
2018-07-26 17:08:15 +1000

    Applied isActive logic to sub categories

--- app/components/Products/LayeredNavigation/SubCategoriesFilter.jsx
+++ app/components/Products/LayeredNavigation/SubCategoriesFilter.jsx
@@ -7,16 +7,26 @@ export function SubCategoriesFilter(props) {
     const { toggleQueryHandler } = props;
     toggleQueryHandler(slug, termId);
   };
-  const { subcategories } = props;
+  const { subcategories, currentParams } = props;
   if (subcategories.length <= 0) return null;
+  const subCategorySlug = 'category';
   return (
     <div className="filter-block subcategories">
       <span className="title">Subcategories</span>
       <ul className="options">
         { subcategories.map((subcategory) => {
+          const termId = subcategory.term_id.toString();
+          const keyExists = Object.prototype.hasOwnProperty.call(currentParams, subCategorySlug);
+          const isArray = keyExists && Array.isArray(currentParams[subCategorySlug]);
+          let isActive = false;
+          if (keyExists && isArray) {
+           isActive = currentParams[subCategorySlug].includes(termId);
+         } else if (keyExists && !isArray) {
+           isActive = currentParams[subCategorySlug] === termId;
+         }
           return (
-            <li className="option subcategory" key={subcategory.name}>
-              <NavLink to="#" onClick={event => toggleSubCategory(event, 'category', subcategory.term_id)}>
+            <li className={`option subcategory ${isActive ? 'active' : ''}`} key={subcategory.name}>
+              <NavLink to="#" onClick={event => toggleSubCategory(event, subCategorySlug, subcategory.term_id)}>
                 <span>{subcategory.name}</span>
               </NavLink>
             </li>
