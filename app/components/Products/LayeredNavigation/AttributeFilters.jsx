ad15f426e732c8d4d3b16fe11bfa3da08bb08c98
Allan Pooley <allanpooley@outlook.com>
2018-07-26 13:22:39 +1000

    Massive overhaul to Attribute Filters

--- app/components/Products/LayeredNavigation/AttributeFilters.jsx
+++ app/components/Products/LayeredNavigation/AttributeFilters.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
-import { AttributeFilter } from './AttributeFilter';
+import { NavLink } from 'react-router-dom';
+
+const AttributeFilter = (props) => {
+  const selectFilter = (event, slug, termId) => {
+    event.preventDefault();
+    const { changeQueryHandler } = props;
+    changeQueryHandler({ [slug]: termId });
+  };
+  const { attribute } = props;
+  if (!attribute.options) return null;
+  return (
+    <div className={`filter-block ${attribute.slug}`}>
+      <span className="title">{attribute.label}</span>
+      <ul>
+        { attribute.options &&
+          attribute.options.map((option) => {
+            return (
+              <li key={option.name}>
+                <NavLink to="#" onClick={event => selectFilter(event, attribute.slug, option.term_id)}>{option.name}</NavLink>
+              </li>
+            );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export const AttributeFilters = (props) => {
   const {
     filters,
     filterType,
-    urlBase,
-    location
+    changeQueryHandler
   } = props;
-  // For attributes we loop over all attributes and render each one in its
-  // own separate filter block
   return Object.keys(filters[filterType]).map((attribute) => {
     const attributeDetails = filters[filterType][attribute];
     return (
       <AttributeFilter
         attribute={attributeDetails}
-        urlBase={urlBase}
-        location={location}
-        key={attribute}
+        changeQueryHandler={changeQueryHandler}
       />
     );
   });
