be5c3f0049dabe6b306ad1789249fe53a8421c2b
Allan Pooley <allanpooley@outlook.com>
2018-07-26 13:22:39 +1000

    Massive overhaul to Attribute Filters

--- app/components/Products/LayeredNavigation/LayeredNavigation.jsx
+++ app/components/Products/LayeredNavigation/LayeredNavigation.jsx
@@ -1,64 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import qs from 'query-string';
+import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
-import PriceSlider from './PriceSlider';
+import { PriceSlider } from './PriceSlider';
 import { SubCategoriesFilter } from './SubCategoriesFilter';
 import { AttributeFilters } from './AttributeFilters';
 import { Title } from '../../Content/Title';
 
-export const LayeredNavigation = (props) => {
-  const {
-    filters,
-    location,
-    categoryName,
-    urlBase
-  } = props;
-  const { attributes, subcategories, price } = filters;
-  const hasAttributeFilters = attributes.some((attribute) => {
-    return attribute.options != null;
-  });
-  const hasSubCategories = typeof subcategories !== 'undefined' && subcategories.length > 0;
-  const hasPriceRange = price.min_price !== price.max_price;
-  if (hasAttributeFilters || hasPriceRange || hasSubCategories) {
-    return (
-      <aside className="layered-navigation">
-        <Title title={categoryName} />
-        <div className="header">
-          <span className="title">Refine</span>
-          <NavLink to={`/${urlBase}`}>Clear all</NavLink>
-        </div>
-        {Object.keys(filters).map((filterType) => {
-          if (filterType === 'price') {
-            return (
-              <PriceSlider
-                key={filterType}
-                filter={filters[filterType]}
-                location={location}
-              />
-            );
-          }
-          if (filterType === 'attributes') {
-            return (
-              <AttributeFilters
-                key={filterType}
-                filters={filters}
-                filterType={filterType}
-                location={location}
-              />
-            );
-          }
-          if (filterType === 'subcategories') {
-            return (
-              <SubCategoriesFilter
-                key={filterType}
-                subcategories={filters[filterType]}
-                location={location}
-              />
-            );
-          }
-          return null;
-        })}
-      </aside>
-    );
+class LayeredNavigation extends Component {
+  changeQueries = (newParams) => {
+    const { history, location } = this.props;
+    const currentParams = qs.parse(location.search);
+    history.push({
+      search: qs.stringify({
+        ...currentParams,
+        ...newParams
+      })
+    });
   }
-  return null;
-};
+
+  render() {
+    const {
+      filters,
+      location,
+      history,
+      categoryName,
+      urlBase
+    } = this.props;
+    const { attributes, subcategories, price } = filters;
+    const hasAttributeFilters = attributes.some((attribute) => {
+      return attribute.options != null;
+    });
+    const currentParams = qs.parse(location.search);
+    const hasSubCategories = typeof subcategories !== 'undefined' && subcategories.length > 0;
+    const hasPriceRange = price.min_price !== price.max_price;
+    if (hasAttributeFilters || hasPriceRange || hasSubCategories) {
+      return (
+        <aside className="layered-navigation">
+          <Title title={categoryName} />
+          <div className="header">
+            <span className="title">Refine</span>
+            <NavLink to={`/${urlBase}`}>Clear all</NavLink>
+          </div>
+          {Object.keys(filters).map((filterType) => {
+            if (filterType === 'price') {
+              return (
+                <PriceSlider
+                  key={filterType}
+                  filter={filters[filterType]}
+                  currentParams={currentParams}
+                  history={history}
+                  changeQueryHandler={this.changeQueries}
+                />
+              );
+            }
+            if (filterType === 'attributes') {
+              return (
+                <AttributeFilters
+                  key={filterType}
+                  filters={filters}
+                  filterType={filterType}
+                  location={location}
+                  changeQueryHandler={this.changeQueries}
+                />
+              );
+            }
+            if (filterType === 'subcategories') {
+              return (
+                <SubCategoriesFilter
+                  key={filterType}
+                  subcategories={filters[filterType]}
+                  location={location}
+                  currentParams={currentParams}
+                />
+              );
+            }
+            return null;
+          })}
+        </aside>
+      );
+    }
+    return null;
+  }
+}
+
+export default withRouter(LayeredNavigation);
--- app/components/Products/LayeredNavigation/PriceSlider.jsx
+++ app/components/Products/LayeredNavigation/PriceSlider.jsx
@@ -1,63 +1,37 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router';
-import qs from 'query-string';
-import Tooltip from 'rc-tooltip';
+import React from 'react';
 import Slider from 'rc-slider';
 import './PriceSlider.scss';
 
-const { createSliderWithTooltip, Handle } = Slider;
+const { createSliderWithTooltip } = Slider;
 const Range = createSliderWithTooltip(Slider.Range);
 
-const handle = (props) => {
-  const {
-    value,
-    dragging,
-    index,
-    ...restProps
-  } = props;
-  return (
-    <Tooltip
-      prefixCls="rc-slider-tooltip"
-      overlay={value}
-      visible={dragging}
-      placement="top"
-      key={index}
-    >
-      <Handle value={value} {...restProps} />
-    </Tooltip>
-  );
-};
-
-class PriceSlider extends Component {
-  handleChange = (newRange) => {
-    const { history, location } = this.props;
+export const PriceSlider = (props) => {
+  const changeRange = (newRange, prevRange) => {
+    const { changeQueryHandler } = props;
+    const prevMinPrice = prevRange[0];
+    const prevMaxPrice = prevRange[1];
     const newMinPrice = newRange[0];
     const newMaxPrice = newRange[1];
-    const currentParams = qs.parse(location.search);
-    history.push({
-      search: qs.stringify({
-        ...currentParams,
-        min_price: newMinPrice,
-        max_price: newMaxPrice
-      })
-    });
-  }
-
-  render() {
-    const { filter, location } = this.props;
-    const { min_price: minPrice, max_price: maxPrice } = filter;
-    const rangeMin = minPrice ? parseInt(minPrice) : 0;
-    const rangeMax = minPrice ? parseInt(maxPrice) : 0;
-    const queryParams = qs.parse(location.search);
-    const currMinPrice = queryParams.min_price ? parseInt(queryParams.min_price) : rangeMin;
-    const currMaxPrice = queryParams.max_price ? parseInt(queryParams.max_price) : rangeMax;
-    return (
-      <section className="filter-block price-slider">
-        <span className="title">Price</span>
-        <Range min={rangeMin} max={rangeMax} defaultValue={[currMinPrice, currMaxPrice]} tipFormatter={value => `$${value}`} onAfterChange={newRange => this.handleChange(newRange)} />
-      </section>
-    );
-  }
-}
-
-export default withRouter(PriceSlider);
+    if (prevMinPrice !== newMinPrice || prevMaxPrice !== newMaxPrice) {
+      changeQueryHandler({ min_price: newMinPrice, max_price: newMaxPrice });
+    }
+  };
+  const { filter, currentParams } = props;
+  const { min_price: minPrice, max_price: maxPrice } = filter;
+  const rangeMin = minPrice ? parseInt(minPrice) : 0;
+  const rangeMax = minPrice ? parseInt(maxPrice) : 0;
+  const currMinPrice = currentParams.min_price ? parseInt(currentParams.min_price) : rangeMin;
+  const currMaxPrice = currentParams.max_price ? parseInt(currentParams.max_price) : rangeMax;
+  const currRange = [currMinPrice, currMaxPrice];
+  return (
+    <section className="filter-block price-slider">
+      <span className="title">Price</span>
+      <Range
+        min={rangeMin}
+        max={rangeMax}
+        defaultValue={currRange}
+        tipFormatter={value => `$${value}`}
+        onAfterChange={newRange => changeRange(newRange, currRange)} />
+    </section>
+  );
+};
