61c886005236b9dff955cabfc68c9af0d1b3fd08
Allan Pooley <allanpooley@outlook.com>
2018-08-14 09:53:51 +1000

    Removed Modal close confirmation handlers

--- app/components/Common/Modal.jsx
+++ app/components/Common/Modal.jsx
@@ -10,24 +10,12 @@ export default class Modal extends Component {
   componentDidMount() {
     if (isClient) {
       document.addEventListener('keydown', this.handleEscKey, false);
-      window.addEventListener('beforeunload', this.onUnload);
-    }
-  }
-  componentWillReceiveProps(nextProps) {
-    if (this.props.active && !nextProps.active) {
-      this.onUnload();
     }
   }
   componentWillUnmount() {
     if (isClient) {
       document.removeEventListener('keydown', this.handleEscKey, false);
-      window.removeEventListener('beforeunload', this.onUnload);
     }
-  }
-  onUnload = (event) => {
-    const promptConfirm = document.querySelector('.loading') || this.props.warnBeforeClose;
-    // eslint-disable-next-line
-    if (promptConfirm && event) event.returnValue = 'Are you sure?';
   }
   handleEscKey = (event) => {
     if (event.keyCode === 27) this.props.closeModal(event);
