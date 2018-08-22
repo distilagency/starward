b242c1797e5e710f8f7cd05a11d728abb6233e2c
samlogan <sam@sjlwebdesign.com>
2018-08-22 17:24:44 +1000

    More inclusive Regex for blacklist

--- server/config/app-template.js
+++ server/config/app-template.js
@@ -8,4 +8,4 @@ export const GRAVITY_PRIVATE = 'Add private key from gravity forms settings';
 export const REDIS_PREFIX = null; // change me to your project name, for example: 'starward-'
 
 // General
-export const characterBlacklistRegex = /[^\w\s]/gi;
+export const characterBlacklistRegex = /[^a-z0-9\\[\\]]/gi;
