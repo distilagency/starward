1a821975b5ff727911c00a88eed33ade88570cd3
Allan Pooley <allanpooley@outlook.com>
2018-09-24 17:03:43 +1000

    Began reworking the MiniCart

--- app/components/Cart/MiniCart.jsx
+++ app/components/Cart/MiniCart.jsx
@@ -1,47 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { MiniCartContent } from './MiniCartContent';
 import { fetchCart, removeFromCart } from '../../actions/cart';
 import './MiniCart.scss';
 
 class MiniCart extends Component {
-  state = {
-    active: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: props.cart.items
+    };
+  }
   componentDidMount() {
+    // eslint-disable-next-line
     const { fetchCart } = this.props;
     fetchCart();
   }
+  componentDidUpdate(prevProps) {
+    const { cart } = this.props;
+    const { cart: prevCart } = prevProps;
+    const {
+      items,
+      loadingItems
+    } = cart;
+    const {
+      items: prevItems
+    } = prevCart;
+    if ((prevItems !== items) && !loadingItems) {
+      this.setState({ items }); // eslint-disable-line
+    }
+  }
   removeFromCartHandler = (event, itemKey) => {
     event.preventDefault();
-    const { removeFromCart } = this.props;
+    const { removeFromCart } = this.props; // eslint-disable-line
     removeFromCart(itemKey);
-  }
-  toggleMiniCart = (event) => {
-    event.preventDefault();
-    const { active } = this.state;
+    // Remove item from local state so it is immediately removed from frontend
+    const { items } = this.state;
+    const newItems = items.filter(item => item.key !== itemKey);
     this.setState({
-      active: !active
+      items: newItems
     });
   }
   render() {
-    const { cart } = this.props;
-    const { active } = this.state;
-    const { items } = cart;
+    const { active, toggleMiniCartHandler } = this.props;
+    const { items } = this.state;
     return (
-      <div className="mini-cart">
-        <NavLink to="#" className="mini-cart-button" onClick={event => this.toggleMiniCart(event)}>
-          { items.length > 0 &&
-            <div className="count-bubble">
-              {items.length}
-            </div>
-          }
-        </NavLink>
-        <div className={`cart-dropdown ${active && 'active'}`}>
+      <div className={`mini-cart ${active ? 'active' : ''}`}>
+        <Link
+          to="#open-minicart"
+          className="mini-cart-button"
+          onClick={event => toggleMiniCartHandler(event)}>
+          { items && items.length > 0 && <div className="count-bubble">{items.length}</div> }
+        </Link>
+        <div className="cart-dropdown">
           <MiniCartContent
             items={items}
             removeFromCartHandler={this.removeFromCartHandler}
+            toggleMiniCartHandler={toggleMiniCartHandler}
           />
         </div>
       </div>
